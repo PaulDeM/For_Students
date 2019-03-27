7580f0efebf08cdc007bd5252277c0b5ec4b8ff9
Paul <pawel.mordasiewicz@wp.pl>
2019-03-27 19:31:39 +0100

    spotkanie nr 2

--- spotkanie_1.R
+++ spotkanie_1.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/Pawe³/Desktop/R_for_Students")
-getwd()
+getwd() #working directory
 dir()
 dir.create("nowy folder")
 dir.exists("nowy folder")
@@ -12,3 +12,58 @@ dir.create("./nowy folder/folderdalej")
 dir.create("./nowy folder/folderdalej/folderjeszczedalej")
 #komentarz po add w git'cie
 #nastepny
+# pierwsza linia
+# druga linia
+# trzecia linia
+#funkcje pomocy
+help()
+help('hist')
+?hist
+example(plot)
+apropos('test')
+help.search('normality test')
+# probny wektor
+Dana<-c(1,2,3,3,4,5,6,6,7,7,7)
+hist(Dana, col='red')
+plot(Dana, col='blue')
+#pakiety
+installed.packages()
+#dzialania matematyczne
+1-1
+1+1
+1-1+4
+2/2
+3/3
+3/2
+3*3
+3**3
+3^2
+sqrt(9)
+16**(1/4)
+#przypisanie
+x=2
+x<-21
+x
+y=3
+x*y
+27->x
+x
+x<-y<-35
+x
+y
+assign('variable', 12)
+variable
+rm(variable)
+variable
+#typy danych
+class(x)
+y<-'Zmienna'
+class(y)
+f<-factor('Zmienna')
+is.factor(f)
+nchar(y)
+nchar(1234)
+v<-c(1,2,34)
+nchar(v)
+nazwaUczelni<-c('S', 'W', 'P', 'S')
+nazwaUczelni_factor<-factor(nazwaUczelni)
